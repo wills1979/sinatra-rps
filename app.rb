3231b36b655607af5a5946eb87fdfd716464e67b
wills1979 <107662428+wills1979@users.noreply.github.com>
2025-04-12 17:55:32 +0000

    add moves

--- app.rb
+++ app.rb
@@ -6,3 +6,63 @@ get("/") do
   erb(:home)
 
 end
+
+get("/rock") do
+
+  moves = ["rock", "paper", "scissors"]  
+
+  @our_move = "rock"
+
+  @computer_move = moves.sample
+  
+  if @computer_move == @our_move
+    @result = "tied"
+  elsif @computer_move == "paper"
+    @result = "lost"
+  else
+    @result = "won"
+  end
+
+  erb(:move)
+
+end
+
+get("/paper") do
+
+  moves = ["rock", "paper", "scissors"]  
+
+  @our_move = "paper"
+
+  @computer_move = moves.sample
+  
+  if @computer_move == @our_move
+    @result = "tied"
+  elsif @computer_move == "scissors"
+    @result = "lost"
+  else
+    @result = "won"
+  end
+
+  erb(:move)
+
+end
+
+get("/scissors") do
+
+  moves = ["rock", "paper", "scissors"]  
+
+  @our_move = "scissors"
+
+  @computer_move = moves.sample
+  
+  if @computer_move == @our_move
+    @result = "tied"
+  elsif @computer_move == "rock"
+    @result = "lost"
+  else
+    @result = "won"
+  end
+
+  erb(:move)
+
+end
